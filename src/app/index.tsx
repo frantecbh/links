bb7c573798c4cef9723177d603488a46d0e5366f
frantecbh <franciscomenezesbh@gmail.com>
2024-09-27 16:43:04 -0300

    estrutura de pastas

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native'
+
+export default function Index() {
+  return(
+    <View>
+      <Text>Ola Frantec!</Text>
+      <Text>Meu app!</Text>
+    </View>
+  )
+}
