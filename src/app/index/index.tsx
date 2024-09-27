d71f11a3564d610c1ea255db2eb9e082bb9d727f
frantecbh <franciscomenezesbh@gmail.com>
2024-09-27 17:30:04 -0300

    header tela inicial

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,11 +1,21 @@
-import { Text, View} from 'react-native'
+import { Image, TouchableOpacity, View} from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+
 import { styles } from './styles'
+import { colors } from '@/styles/colors'
+
+
+
 
 export default function Index() {
   return(
     <View style={styles.container}>
-      <Text style={styles.title}>Ola!</Text>
-      <Text style={styles.title}> Frantec!</Text>
+        <View style={styles.header}>
+            <Image style={styles.logo} source={require('@/assets/logo.png')} />
+            <TouchableOpacity activeOpacity={0.8}>
+            <MaterialIcons name='add' size={32} color={colors.green[300]} />
+            </TouchableOpacity>
+        </View>
     </View>
   )
 }
